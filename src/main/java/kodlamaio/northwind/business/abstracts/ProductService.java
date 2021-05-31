d57fc2aeaac82b53b9f24a26979bcea638244966
Cigdem <atk.cgdm@gmail.com>
2021-05-31 23:15:03 +0300

    7.Day added codes

--- src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -2,10 +2,17 @@ package kodlamaio.northwind.business.abstracts;
 
 import java.util.List;
 
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
 
 public interface ProductService {
-	
-	List<Product> getAll();
+
+	DataResult<List<Product>> getAll();
+
+	Result add(Product product);
 
 }
+
+// Business -> iş katmanımızda controller 'imizin kullanacağı servisleri yazıyoruz
+// Business katmanında sadece iş kodları yazılır
--- src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -1,39 +1,45 @@
 package kodlamaio.northwind.entities.concretes;
 
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+//Entity katmanımız yani varlık katmanımız
 @Table(name = "products")
+//Product nesnesi/classı veritabanındaki "products" tablosuna karşılık gelecek şekilde yapılandırıldı
+//persistance ile gelen @Table notasyanonunu kullanıldı
+//java -> annotation     c# -> attribute 
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
-	
+
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "product_id")
 	private int id;
-	
+
 	@Column(name = "category_id")
 	private int categoryId;
-	
+
 	@Column(name = "product_name")
 	private String productName;
-	
+
 	@Column(name = "unit_price")
 	private double unitPrice;
-	
+
 	@Column(name = "units_in_stock")
 	private short unitsInStock;
-	
+
 	@Column(name = "quantity_per_unit")
 	private String quantityPerUnit;
-	
-	
 
 }
